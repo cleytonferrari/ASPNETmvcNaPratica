06d48b05fc8617320067fbc240b9bac033133bca
Cleyto Ferrari <cleyton@w7br.com>
2013-11-13 09:42:55 -0400

    aula10

---
+++ codigo/aula10/TISelvagem/TISelvagem.Dominio/Aluno.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace TISelvagem.Dominio
+{
+    public class Aluno
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Preencha o nome do aluno")]
+        public string Nome { get; set; }
+        
+        [DisplayName("Mâe")]
+        [Required(ErrorMessage = "Preencha o nome da mãe do aluno")]
+        public string Mae { get; set; }
+
+        [DisplayName("Data de Nascimento")]
+        [Required(ErrorMessage = "Preencha a data de nascimento do aluno")]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MM/yyyy}")]
+        public DateTime DataNascimento { get; set; }
+    }
+}
---
+++ codigo/aula10/TISelvagem/TISelvagem.UI.Web/Controllers/AlunoController.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TISelvagem.Aplicacao;
+using TISelvagem.Dominio;
+
+namespace TISelvagem.UI.Web.Controllers
+{
+    public class AlunoController : Controller
+    {
+        //
+        // GET: /Aluno/
+
+        public ActionResult Index()
+        {
+            var appAluno = new AlunoAplicacao();
+            var listaDeAlunos = appAluno.ListarTodos();
+            return View(listaDeAlunos);
+        }
+
+        public ActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Cadastrar(Aluno aluno)
+        {
+            if (ModelState.IsValid)
+            {
+                var appAluno = new AlunoAplicacao();
+                appAluno.Salvar(aluno);
+                return RedirectToAction("Index");
+            }
+            return View(aluno);
+        }
+
+        public ActionResult Editar(int id)
+        {
+            var appAluno = new AlunoAplicacao();
+            var aluno = appAluno.ListarPorId(id);
+
+            if (aluno == null)
+                return HttpNotFound();
+
+            return View(aluno);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Editar(Aluno aluno)
+        {
+            if (ModelState.IsValid)
+            {
+                var appAluno = new AlunoAplicacao();
+                appAluno.Salvar(aluno);
+                return RedirectToAction("Index");
+            }
+            return View(aluno);  
+        }
+
+        public ActionResult Detalhes(int id)
+        {
+            var appAluno = new AlunoAplicacao();
+            var aluno = appAluno.ListarPorId(id);
+
+            if (aluno == null)
+                return HttpNotFound();
+
+            return View(aluno);
+        }
+
+        public ActionResult Excluir(int id)
+        {
+            var appAluno = new AlunoAplicacao();
+            var aluno = appAluno.ListarPorId(id);
+
+            if (aluno == null)
+                return HttpNotFound();
+
+            return View(aluno);
+        }
+
+        [HttpPost, ActionName("Excluir")]
+        [ValidateAntiForgeryToken]
+        public ActionResult ExcluirConfirmado(int id)
+        {
+            var appAluno = new AlunoAplicacao();
+            appAluno.Excluir(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
